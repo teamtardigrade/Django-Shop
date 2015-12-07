e0904ab35051c7aee0b20473db24cfafab0852ee
andresjp <pydev2015@gmail.com>
2015-12-07 20:35:25 -0300

    category api urls

--- DjangoShop/apps/category/urls.py
+++ DjangoShop/apps/category/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import include, url
+from category import api
 
 app_name = 'category'
 
 urlpatterns = [
     url(r'^all/','views.CategoryCollection',name='CategoryCollection'),
     url(r'^see/(?P<id>\d+)','views.SingleCategory',name='SingleCategory'),
+
+    #REST URLS
+    url(r'', api.CategoryList.as_view()),
+    url(r'^category/(?P<pk>[0-9]+)/$', api.CategoryDetail.as_view()),
 ]
